fd8db971507e4204ba7701b138ed968e292b5039
Estefano22 <sandulescustefano8@gmail.com>
2020-11-26 11:10:39 +0100

    Initial commit

---
+++ app/src/main/java/com/example/examen_1_evalucion/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.examen_1_evalucion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
